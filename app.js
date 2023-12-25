7670b6f277de93cfd6c9737f849fa895b26a0ecd
reymoonwolf <124702018+reymoonwolf@users.noreply.github.com>
2023-12-25 23:03:51 +0400

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 //firstName = 'Maksim'
 //console.log('Hello', firstName)
 
-let firstName = 'Andrew'
-firstName = 'Maksim'
-console.log('Hello', firstName)
+let firstName = 'Андрей'
+let lastName = 'Волков'
+let age = '32'
+let city = 'Саратов'
+let hobby = 'Круто когда твоя работа - это и есть твое хобби!'
+
+
+console.log('Имя:', firstName)
+console.log('Фамилия:', lastName)
+console.log('Возраст:', age)
+console.log('Город:', city)
+console.log('Хобби:', hobby)
